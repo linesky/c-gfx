f5781f3bcef94789dc529bd558527d1c46747ad9
sky line <32512284+linesky@users.noreply.github.com>
2022-04-28 14:17:24 +0100

    bash gx.sh gxx.c

--- gxx.c
+++ gxx.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//gcc  gx.c -L ./ -l gx -lfb -lfbgfx -o gx
+//bash gx.sh gxx.c
 void screens(int x ,int y,int colors) ;
 void box(int x ,int y,int w ,int h,int colors);
 void scroff();
